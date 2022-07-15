235bdaf138ab2e21edc45b9d63608bcbf0366be3
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-15 15:46:27 -0400

    Finsihed exercise

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using StaticExercise;
+var Celsius = TempConverter.FahrToCel(212);
+var Celsius2 =TempConverter.FahrToCel(100);
+var Fahrenheit = TempConverter.CelToFahr(100);
+var Fahrenheit2 = TempConverter.CelToFahr(212);
+
+
+
+Console.WriteLine($"Fahrenheit {Fahrenheit}");
+Console.WriteLine($"Fahrenheit {Fahrenheit2}");
+Console.WriteLine($"Celsius {Celsius}");
+Console.WriteLine($"Celsius {Celsius2}");
---
+++ TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise;
+
+public static class TempConverter
+{
+    public static double FahrToCel(double Fahr)
+    {
+        var result = (Fahr - 32) / 1.8;
+        return result;
+    }
+    public static double CelToFahr(double Cel)
+    {
+        double result = ((9 / 5) * Cel) + 32;
+        return result;
+    }
+}
